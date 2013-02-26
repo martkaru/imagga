c1180cfc051397df6fab8fcb62ed49da2965645f
JP Rosevear <jpr@jprosevear.org>
2013-02-25 23:36:17 -0500

    Build crop info

--- lib/imagga/crop_result_builder.rb
+++ lib/imagga/crop_result_builder.rb
@@ -4,7 +4,7 @@ module Imagga
 
   class CropResultBuilder
     def build_from(result)
-      result['smart_croppings'].map { |image_node| ImageInfo.new(image_node) }
+      result['smart_croppings'].map { |image_node| CropInfo.new(image_node) }
     end
   end
 
