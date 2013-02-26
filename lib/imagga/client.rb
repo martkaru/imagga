ad836b03b1b7b80bb4e70b7513d309051c4bc079
JP Rosevear <jpr@jprosevear.org>
2013-02-25 22:21:06 -0500

    Set up different server paths for color extraction and cropping

--- lib/imagga/client.rb
+++ lib/imagga/client.rb
@@ -5,26 +5,27 @@ module Imagga
   class Client
     include HTTParty
     include Imagga::Exceptions
-    attr_reader :api_key, :api_secret, :base_uri, :service_path
+    attr_reader :api_key, :api_secret, :base_uri, :extract_service_path, :crop_service_path
 
     def initialize(opts={})
       @api_key     = opts[:api_key]    || raise_missing(:api_key)
       @api_secret  = opts[:api_secret] || raise_missing(:api_secret)
       @base_uri    = opts[:base_uri]   || raise_missing(:base_uri)
-      @service_path = '/colorsearchserver.php'
+      @extract_service_path = '/colorsearchserver.php'
+      @crop_service_path = '/extractionrestserver.php'
 
       Imagga::Client.base_uri @base_uri
     end
 
     def extract(urls_or_images, additional_options={})
       options = extract_options(urls_or_images, additional_options)
-      result = JSON.parse(self.class.post(service_path, body: options))
+      result = JSON.parse(self.class.post(extract_service_path, body: options))
       raise_if_request_failed!(result)
       ExtractResultBuilder.new.build_from(result)
     end
 
     def rank(opts)
-      result = JSON.parse(self.class.post(service_path, body: rank_options(opts)))
+      result = JSON.parse(self.class.post(extract_service_path, body: rank_options(opts)))
       raise_if_request_failed!(result)
       RankResultBuilder.new.build_from(result)
     end
